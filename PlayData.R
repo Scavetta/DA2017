6b8857bbe108c7acf600a50674d74600996cec6e
Scavetta <rickyjoey@gmail.com>
2018-06-12 11:59:25 +0200

    Add PlayData.R file

---
+++ PlayData.R
@@ -0,0 +1,5 @@
+# Create a new play dataset to work on:
+PlayData <- data.frame(type = rep(c("A", "B"), each = 2),
+                       time = 1:2,
+                       height = seq(10, 40, 10),
+                       width = seq(50, 80, 10))
